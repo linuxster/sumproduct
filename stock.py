ac3c040a8e79ef3268af270a9938a8462269ff8f
zhk <zhangkangj@gmail.com>
2013-12-13 14:27:33 -0500

    stock

---
+++ stock.py
@@ -0,0 +1,18 @@
+'''
+Created on Dec 11, 2013
+
+@author: zhk
+'''
+import MySQLdb
+
+DB_HOST = '128.31.7.128'
+DB_PORT = 3306
+DB_NAME = 'stock'
+DB_USER = 'zhk'
+DB_PASS = 'G0373485x'
+
+class Stock(object):
+    def __init__(self, ticker):
+        conn = MySQLdb.connect(host = DB_HOST, user = DB_USER, passwd = DB_PASS, db = DB_NAME, port = 3306)
+        
+        
